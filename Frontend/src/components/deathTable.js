d9372617168acfec77bd7992d124070ca9793090
james <jamesliwood@gmail.com>
2024-01-09 02:35:30 -0500

    Revert "fixed spelling"
    This reverts commit 3ce894af615f88d913be3ac165c5a95a70aa34a6.

--- Frontend/src/components/deathTable.js
+++ Frontend/src/components/deathTable.js
@@ -21,7 +21,7 @@ const DeathTable = (props) => {
           <th>Died On</th>
           <th>Base Fame</th>
           <th>Total Fame</th>
-          <th>Equipments</th>
+          <th>Equipment</th>
           <th>Stats</th>
           <th>Killed by</th>
         </tr>
