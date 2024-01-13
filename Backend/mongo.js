7825622abdde11593735576a7f29b89a07de0445
james <jamesliwood@gmail.com>
2024-01-13 18:06:57 -0500

    reverted

--- Backend/mongo.js
+++ Backend/mongo.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 let connection;
 let uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@realmscrape.wmw2fat.mongodb.net/?retryWrites=true&w=majority`;
+console.log(process.env.DB_USERNAME);
 
 module.exports = {
   connectToDb: (callback) => {
