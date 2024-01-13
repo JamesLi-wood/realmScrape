6b708f2b892cafd487c8bd7c62442f14bbbb8c5e
james <jamesliwood@gmail.com>
2024-01-13 18:09:39 -0500

    removed test case

--- Backend/mongo.js
+++ Backend/mongo.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 let connection;
 let uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@realmscrape.wmw2fat.mongodb.net/?retryWrites=true&w=majority`;
-console.log(process.env.DB_USERNAME);
 
 module.exports = {
   connectToDb: (callback) => {
