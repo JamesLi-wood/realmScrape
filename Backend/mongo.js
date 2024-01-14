8f9eed47966a43850180d250c223582a85eaf3c3
james <jamesliwood@gmail.com>
2024-01-14 04:14:20 -0500

    removed test case

--- Backend/mongo.js
+++ Backend/mongo.js
@@ -3,14 +3,13 @@ const { MongoClient } = require("mongodb");
 let connection;
 module.exports = {
   connectToDb: (callback) => {
-    console.log(process.env.URI)
     MongoClient.connect(process.env.URI)
       .then((client) => {
         connection = client.db("websiteScrape");
         return callback();
       })
       .catch((err) => {
-        console.log("Connection Failed." + err);
+        console.log(`Connection Failed. ${err}`);
         return callback(err);
       });
   },
