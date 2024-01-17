2aa61f6d1002c4eab9cc0a41892f266f2da5d8a2
james <jamesliwood@gmail.com>
2024-01-17 02:41:28 -0500

    cleaned up code

--- Backend/app.js
+++ Backend/app.js
@@ -1,98 +1,39 @@
-const webScrape = require("./scrape");
-const { connectToDb, getDb } = require("./mongo");
-const { ping } = require("./ping");
+const { getTopCharacters } = require("./utils/scrape");
+const { connectToDb, checkNewEntries, dbGetSorted } = require("./utils/mongo");
+const { ping } = require("./utils/ping");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
-const cors = require("cors");
-/**
- * @param {Object} query
- * Query value can either be -1 or 1.
- */
-async function dbLowestHighest(collection, query) {
-  return await db
-    .collection(collection)
-    .findOne({}, { sort: query })
-    .then((data) => {
-      return data;
-    });
-}
 
-// Get database connection.
-let db;
-connectToDb((err) => {
-  if (!err) {
-    app.listen(port, () => {
-      console.log(`App listening on port ${port}`);
-    });
-    db = getDb();
-  }
-});
-
+/* Pings server every 14 minutes so the server 
+   doesn't go inactive on Render. */
 ping();
 
-setInterval(async () => {
-  const websiteRecent = await webScrape.getRecentDeath();
-  const dbRecent = await dbLowestHighest("recentDeaths", { deathDate: -1 });
+connectToDb();
 
-  if (
-    websiteRecent.deathDate !== dbRecent.deathDate &&
-    websiteRecent.name !== "Private"
-  ) {
-    const lowestFame = await dbLowestHighest("topDeaths", { baseFame: 1 });
-    if (websiteRecent.baseFame > lowestFame.baseFame) {
-      await db
-        .collection("topDeaths")
-        .updateOne({ _id: lowestFame._id }, { $set: websiteRecent });
-      console.log("A new character has made it in the topDeaths leaderboard!");
-    }
+/* Sets an interval every 10 seconds to check if
+   a new death has been posted. */
+checkNewEntries();
 
-    await db.collection("recentDeaths").deleteOne({
-      _id: await dbLowestHighest("recentDeaths", { deathDate: 1 }).then(
-        (data) => {
-          return data._id;
-        }
-      ),
-    });
-    await db.collection("recentDeaths").insertOne(websiteRecent);
-    console.log("A character has been added to the graveyard!");
-  }
-}, 10000);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
 
-app.use(cors());
+app.use(cors()); // Enables CORS.
 
 app.get("/", (req, res) => {
   res.end("Welcome to realm scrape!");
 });
 
 app.get("/recentDeaths", async (req, res) => {
-  const characters = [];
-  await db
-    .collection("recentDeaths")
-    .find()
-    .sort({ deathDate: -1 })
-    .forEach((chars) => {
-      characters.push(chars);
-    })
-    .then(() => {
-      res.send(characters);
-    });
+  res.send(await dbGetSorted("recentDeaths", { deathDate: -1 }));
 });
 
 app.get("/topDeaths", async (req, res) => {
-  const characters = [];
-  await db
-    .collection("topDeaths")
-    .find()
-    .sort({ baseFame: -1 })
-    .forEach((chars) => {
-      characters.push(chars);
-    })
-    .then(() => {
-      res.send(characters);
-    });
+  res.send(await dbGetSorted("topDeaths", { baseFame: -1 }));
 });
 
 app.get("/topCharacters", async (req, res) => {
-  res.send(await webScrape.getTopCharacters());
+  res.send(await getTopCharacters());
 });
